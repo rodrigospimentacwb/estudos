fe55f548c464ba218d9766baf101f4cb732ae44f
rodrigospimentacwb <rodrigospimenta@gmail.com>
2021-08-17 23:53:43 -0300

    Initial Commit

---
+++ simpleSpringProject/src/test/java/br/com/pepper/estudo/simpleSpringProject/SimpleSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pepper.estudo.simpleSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
